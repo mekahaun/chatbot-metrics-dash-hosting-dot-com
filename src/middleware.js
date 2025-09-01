47dd93d5b13adcb353618493e2e45df808fc5ca7
S M Kaiser Ahmed <kaiser331042@gmail.com>
2025-09-01 23:06:20 +0600

    refactor

--- src/middleware.js
+++ src/middleware.js
@@ -31,4 +31,21 @@ export const config = {
 };
 
 // ====== Without Auth part ======
+// import { NextResponse } from "next/server";
 
+// export default function middleware(req) {
+//   const { pathname } = req.nextUrl;
+  
+//   // If user is trying to access home page, redirect to dashboard
+//   if (pathname === "/") {
+//     return NextResponse.redirect(new URL("/dashboard/overview", req.url));
+//   }
+  
+//   return NextResponse.next();
+// }
+
+// export const config = {
+//   matcher: [
+//     "/",
+//   ],
+// };
